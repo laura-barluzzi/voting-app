5fde4ba17a4b35439bf68994bbfb15d7b2e9571c
Laura Barluzzi <laura@koalacoder.com>
2018-02-08 16:53:11 +0000

    adds user option to vote for an option

--- voting-app-frontend/src/PollViewer.js
+++ voting-app-frontend/src/PollViewer.js
@@ -28,13 +28,29 @@ export default class PollViewer extends Component {
     
     this.loadPoll(pollId);
   }
-  
+
   showEditButton = (poll) => 
     <Link to={{ pathname: `/edit/${poll.id}`, state: { poll } }}>Edit</Link>
 
+  addVote = (optionName) => {
+    const { poll } = this.state;
+    poll.options[optionName]++;
+
+    request({
+      uri: `${process.env.REACT_APP_SERVER_HOST}/api/vote`,
+      json: true,
+      method: 'POST',
+      body: { poll },
+    }).then(response => {
+      this.setState({ poll: response.poll, error: null });
+    }).catch(error => {
+      this.setState({ error });
+    });
+  }
+
   render() {
     const { poll } = this.state;
-    
+
     if (!poll) {
       return null;
     }
@@ -46,9 +62,10 @@ export default class PollViewer extends Component {
         <p>Title: {poll.title}</p>
         <p>Creator: {poll.creator}</p>
 
-        {Object.keys(poll.options).map((optionName) =>
+        {Object.keys(poll.options).map((optionName, i) =>
           <p key={optionName}>
-            Option {optionName} has {poll.options[optionName]} votes
+            {i}. {optionName} has {poll.options[optionName]} votes
+            <button onClick={() => this.addVote(optionName)}>vote</button>
           </p>
         )}
       </div>
